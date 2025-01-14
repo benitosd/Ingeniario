307f87ce261371374916dad36443146bdba0cb98
Gabe <benito.santana@gmail.com>
2025-01-14 09:17:57 +0000

    SORTER LIST

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -2,3 +2,4 @@ ITEMS_SORTER_LIST = {"Descripción" => "description_for_sort",
                      "Nombre"  => "name_for_sort"}
 GROUPS_SORTER_LIST = {"Descripción" => "description_for_sort",
                      "Nombre"  => "name_for_sort"}
+USERS_SORTER_LIST = {}
