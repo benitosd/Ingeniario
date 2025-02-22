513f1e0b630eaebc6e99fb8778165695b6f3472e
Gabe <benito.santana@gmail.com>
2025-02-22 18:38:25 +0000

    reportes

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link_tree ../builds
-//= link_tree ../javascripts .js
+//= link_directory ../javascripts .js
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link html5-qrcode.min.js
